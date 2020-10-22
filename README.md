b3b8d8c61f3e4d82dfe012243e3e2e0b2e32f55c
dasileker <dasileker@gmail.com>
2020-10-22 11:29:39 +0100

    refactored the ach method

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# scraper_project
+# Scraper_Project  for Microverse
+
+- in this project I did Scrape a web page for my capstone Project in Ruby.
+
+# Demo Images
+
+*![image](./tic-tac.gif)
+
+
+# Rspec Demo Images
+
+*![image](./tic-tac.gif)
+
+
+
+# This project has four milestones
+- Project setup
+- Scraper logic
+- Instruction How to use it
+
+### Project Structure
+
+```bash 
+├── Gemfile
+├── Gemfile.lock
+├── README.md
+├── scrapee.rb
+└── spec
+    ├── 01_tic_tac_toe_spec.rb
+    ├── 02_cli_spec.rb
+    └── spec_helper.rb
+```
+
--- scraper.rb
+++ scraper.rb
@@ -4,40 +4,38 @@ require 'httparty'
 require 'open-uri'
 require 'csv'
 
-
 def scraper
-  page = "https://www.avito.ma/fr/maroc/v%C3%A9hicules-%C3%A0_vendre?o=1"
+  page = 'https://www.avito.ma/fr/maroc/v%C3%A9hicules-%C3%A0_vendre?o=1'
   unparsed_page = HTTParty.get(page)
   parsed_page = Nokogiri::HTML(unparsed_page)
-  carss = Array.new
+  carss = []
   cars = parsed_page.css('div.listing listing-thumbs, div.ctext2')
-  
+
   cars.each do |auto|
-      auto = {
-        title: auto.css('h2.fs14').text,
-        location: auto.css('div.re-text').text,
-        date: auto.css('span.age-text').text,
-        url: auto.css('a')[0].attributes["href"].value
-      }
-      carss << auto 
-      puts "Added #{auto[:title]},
+    auto = {
+      title: auto.css('h2.fs14').text,
+      location: auto.css('div.re-text').text,
+      date: auto.css('span.age-text').text,
+      url: auto.css('a')[0].attributes['href'].value
+    }
+    carss << auto
+    puts "Added #{auto[:title]},
       #{auto[:location]},
       #{auto[:date]},
       #{auto[:url]}"
-    end
-
+  end
 end
 
 scraper
 
-filepath = "scraper.csv"
+filepath = 'scraper.csv'
 
-csv_option = {headers: :first_row, col_sep: ','}
+csv_option = { headers: :first_row, col_sep: ',' }
 
 CSV.open(filepath, 'wb', csv_option) do |csv|
-  csv << ['title', 'index']
+  csv << %w[title index]
 
   scraper.each_with_index do |item, index|
-    csv <<  [item, index]
+    csv << [item, index]
   end
 end
