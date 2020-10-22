ed9615f81894916e581dc986d30fb75fe453e261
dasileker <dasileker@gmail.com>
2020-10-22 09:55:21 +0100

    added the defeintion for the scrape method

--- scraper.rb
+++ scraper.rb
@@ -3,3 +3,14 @@ require 'pry'
 require 'httparty'
 require 'open-uri'
 require 'csv'
+
+
+def scraper
+  page = "https://www.avito.ma/fr/maroc/v%C3%A9hicules-%C3%A0_vendre?o=1"
+  unparsed_page = HTTParty.get(page)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  carss = Array.new
+  cars = parsed_page.css('div.listing listing-thumbs, div.ctext2')
+  
+
+end
