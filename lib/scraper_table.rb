7198f58a45235bde22e2361de3c9b57567eed986
dasileker <dasileker@gmail.com>
2020-10-23 10:56:58 +0100

    added the visual display to the terminal when the file run

--- lib/scraper_table.rb
+++ lib/scraper_table.rb
@@ -1,1 +1,32 @@
 require 'terminal-table'
+
+
+class ScrapedTable
+  def initialize(page, input, total)
+    @page = page
+    @input = input
+    @totla = total
+    @index = 0
+  end
+
+  def display
+    Terminal::Table.new(
+      row: [
+        [@total, @page, pages_index, user_Query]
+      ],
+      headers: [
+        'result',
+        'page fetched',
+        'page totals',
+        'fetched',
+      ],
+      style: {
+        border_i: '-'
+      }
+    )
+  end
+
+
+
+
+end
