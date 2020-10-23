be32c80039aa7aa3ff0e217ecc800f4acf1e48e7
dasileker <dasileker@gmail.com>
2020-10-23 10:02:55 +0100

    added the  def input like the TSE requsted the OOP methode  in seperate file

--- bin/main.rb
+++ bin/main.rb
@@ -3,3 +3,18 @@
 require 'nokogiri'
 require 'httparty'
 require_relative '../lib/scraper.rb'
+
+
+def input(input)
+  gets.downcase.chomp.gsub(' ', '%20') if input.nil?
+end
+
+def user
+  input = gest.downcase.chomp
+  if input.to_i.zero?
+    input.to_s.gsub(' ', '%20')
+  else
+    input.to_i
+  end
+  input
+end
