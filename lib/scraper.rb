a7c9b3a3b61cab70d0f87e035e48694fbd546137
dasileker <dasileker@gmail.com>
2020-10-23 09:54:49 +0100

    I splited the code in two definition ("counter : to count the item", & "torrent: to return the result of counter")

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,4 +26,34 @@ class Scraper
     @table = div.css('tbody')
   end
 
+  def counter
+    h2 = @vue.css('a.plain').text.split("\n")
+    search = if !h2[1].nil?
+                h2[1].split(' ')
+             else
+              h2.push('0')
+             end
+    @result = search[-1].to_i
+    @result
+  end
+
+  def torrent
+    counter
+    list = []
+    @table.css('tr'.each) do |torren_item|
+      table_list = []
+      href = torren_item.css('div.iaconbox a')
+      @html = href.map { |link| link['href'] }
+      extract_torrent = torren_item.css('div.toeentname').text.to_s.split(/\n/)
+      decrypt = []
+      extract_torrent.each { |text| decrypt << text unless text.empty? }
+      table_list << (@count += 1).to_s
+      decrypt.each { |title_item| table_list << title_item.to_s + "\n" }
+      table_list << "https://katcr.to#{@html[0]}\n"
+      table_list << "-----------------------------------"
+      list << table_list
+    end
+    list
+  end
+
 end
